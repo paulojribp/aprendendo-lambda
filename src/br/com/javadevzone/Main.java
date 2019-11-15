69f6db816baddf06444d9f8319841cde1fecd5ca
Paulo Jr <paulojribp@gmail.com>
2019-11-15 18:10:00 -0300

    Aprendendo Streams e Optionals

--- src/br/com/javadevzone/Main.java
+++ src/br/com/javadevzone/Main.java
@@ -4,25 +4,46 @@ import br.com.javadevzone.modelo.Usuario;
 
 import java.util.*;
 import java.util.function.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        ordenandoUsuarios();
+        usandoOptionalEStream();
+    }
+
+    private static void usandoOptionalEStream() {
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("Jose Silva", 10));
+        usuarios.add(new Usuario("Pedro Cabral", 25));
+        usuarios.add(new Usuario("Reebrtv", 450));
+        usuarios.add(new Usuario("Zebulu", 10));
+        usuarios.add(new Usuario("Qyuaklqyuer", 10));
+        usuarios.add(new Usuario("Josue da Costa", 320));
+        usuarios.add(new Usuario("Henrique Silva", 500));
+
+        OptionalDouble average = usuarios.stream().mapToInt(Usuario::getPontos).average();
+        System.out.println("Soma de todos os pontos: " + average.orElse(0.0));
+
+        Optional<String> nome = usuarios.stream().max(Comparator.comparing(Usuario::getPontos)).map(Usuario::getNome);
+        System.out.println(nome.get());
+
+        usuarios.stream().filter(u -> u.getPontos() > 100).forEach(Usuario::tornarModerador);
+        usuarios.sort(Comparator.nullsLast(Comparator.comparing(Usuario::getPontos).reversed()));
     }
 
     private static void ordenandoUsuarios() {
         List<Usuario> usuarios = new ArrayList<>();
         usuarios.add(new Usuario("Jose Silva", 10));
         usuarios.add(new Usuario("Pedro Cabral", 25));
-        usuarios.add(null);
-        usuarios.add(new Usuario("Teste", 450));
+        usuarios.add(new Usuario("Reebrtv", 450));
         usuarios.add(new Usuario("Zebulu", 10));
         usuarios.add(new Usuario("Qyuaklqyuer", 10));
-        usuarios.add(new Usuario("Josue da Costa", 32));
-        usuarios.add(new Usuario("Henrique Silva", 10));
+        usuarios.add(new Usuario("Josue da Costa", 320));
+        usuarios.add(new Usuario("Henrique Silva", 500));
 
-        usuarios.sort(Comparator.nullsLast(Comparator.comparing(Usuario::getNome).reversed()));
+        usuarios.sort(Comparator.nullsLast(Comparator.comparing(Usuario::getPontos).reversed()));
 
         Usuario u = new Usuario("Paulo Junior", 15);
         BiFunction<String, Integer, Usuario> supplier = Usuario::new;
@@ -34,10 +55,7 @@ public class Main {
         valida.valida();
         System.out.println(u);
 
-        //usuarios.forEach(System.out::println);
-
-        //List<String> tickets = Arrays.asList("PETR4", "VALE3", "ITUB4");
-        //tickets.forEach(s -> System.out.println(s));
+        usuarios.forEach(System.out::println);
     }
 
     public static void runnable() {
